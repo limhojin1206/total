e6f842193a0af8c5a987749b01b34c7112bee00d
1103-12 <1103-12@1103-12-PC>
2017-09-14 12:51:59 +0900

    집에서

---
+++ chap06_TOTAL/src/main/java/org/estrella/app/controller/MyController.java
@@ -0,0 +1,16 @@
+package org.estrella.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/my")
+public class MyController {
+	
+	@GetMapping("/info")
+	public String infoHandle() {
+		return "t_expr";
+	}
+	
+}
